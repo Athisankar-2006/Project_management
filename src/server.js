665e255a9c2073559c2658b82bb93bb3696189f2
Athisankar-2006 <athisankar2k6@gmail.com>
2026-03-06 23:23:14 +0530

    project structure created

---
+++ src/server.js
@@ -0,0 +1,20 @@
+// import express from express;
+import dotenv from "dotenv";
+
+dotenv.config({
+    path :"./.env",
+})
+
+let myname=process.env.username;
+
+console.log("value: ",myname)
+
+// const app=express;
+
+
+
+// const PORT=7000;
+
+// app.use(PORT,)
+
+ console.log("hii")
