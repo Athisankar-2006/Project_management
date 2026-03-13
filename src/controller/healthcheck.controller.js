2c09c7e989dbe29b09df874e49489f84f0c846fe
Athisankar-2006 <athisankar2k6@gmail.com>
2026-03-13 19:51:01 +0530

    model created

--- src/controller/healthcheck.controller.js
+++ src/controller/healthcheck.controller.js
@@ -1,16 +1,23 @@
 import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
-
-const healthCheck=(req,res)=>{
-    try{
-        res
-           .status(200)
-           .json(new ApiResponse(200, {message : " server is running"}))
-    }catch(error){
-
-    };
+// const healthCheck=async (req,res,next)=>{
+//     try{
+//         const user=getUserFromDB();
+//         res
+//            .status(200)
+//            .json(new ApiResponse(200, {message : " server is running"}))
+//     }catch(error){
+//         next(error  )
+//     };
     
-};
+// };
+
+
+const healthCheck=asyncHandler(async(req,res)=>{
+    res.status(200).json(new ApiResponse(200,{message:"server is runnig"}))
+})
+
 
 
 export {healthCheck};
