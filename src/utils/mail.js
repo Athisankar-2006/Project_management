f64920795ef1d271b3768e5a214c64095fa03f80
Athisankar-2006 <athisankar2k6@gmail.com>
2026-03-17 11:53:42 +0530

    user register

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,4 +1,56 @@
 import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+
+
+
+const sendEmail=async(options)=>{
+  const mailGenerator=  new Mailgen({
+        theme: "default",
+        product: {
+            name:"Task manager",
+            link:"https://taskmanagelink.com"
+        }
+    })
+    
+   const mailTextual=mailGenerator.generatePlaintext(options.mailgenContent)
+  const emailHtml=mailGenerator.generate(options.mailgenContent)
+
+   
+  const transport=nodemailer.createTransport({
+    host:process.env.MAILTRAP_SMTP_HOST,
+    port:process.env.MAILTRAP_SMTP_PORT,
+    auth:{
+        user:env.MAILTRAP_SMTP_USER,
+        pass:process.env.MAILTRAP_SMTP_PASS
+    }
+  })
+
+
+
+
+  const mail={
+    from: "mail.taskmanager@example.com",
+    to:options.email,
+    subject:options.subject,
+    text:emailTextual,
+    html:emailHtml
+  }
+
+  try{
+    await transporter.sendMail(mail)
+  }catch(error){
+       console.error("email service failed silently. make sure that you have provide your MAILTRAP credentials in the .env file")
+       console.error("Error:",error)
+  }
+
+
+}
+
+
+
+
+
 
 const emailVerificationMailgenContent=(username,vericationUrl)=>{
     return{
